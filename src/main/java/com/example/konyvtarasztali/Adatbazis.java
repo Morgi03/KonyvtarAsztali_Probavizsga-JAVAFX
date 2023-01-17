a851e724782aad16cb33c82d188bfcf8d038cdd5
Morgován Nimród <morgovannimrod22@gmail.com>
2023-01-17 14:46:27 +0100

    Adatlekérés adatbázisból

--- src/main/java/com/example/konyvtarasztali/Adatbazis.java
+++ src/main/java/com/example/konyvtarasztali/Adatbazis.java
@@ -1,8 +1,8 @@
 package com.example.konyvtarasztali;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Adatbazis {
     private Connection conn;
@@ -17,4 +17,22 @@ public class Adatbazis {
         String url = String.format("jdbc:%s://%s:%s/%s", DB_DRIVER, DB_HOST, DB_PORT, DB_DBNAME);
         conn = DriverManager.getConnection(url, DB_USER, DB_PASS);
     }
+
+    public List<Konyv> readAllBook() throws SQLException {
+        List<Konyv> konyvek = new ArrayList<>();
+        String sql = "SELECT * FROM books";
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet = stmt.executeQuery(sql);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            int publish_year = resultSet.getInt("publish_year");
+            int page_count = resultSet.getInt("page_count");
+            Konyv konyv = new Konyv(id, title, author, publish_year, page_count);
+            konyvek.add(konyv);
+        }
+        return konyvek;
+    }
+
 }
