3f0cfa179002ae30f578a51f9a57512d20bc96da
Morgován Nimród <morgovannimrod22@gmail.com>
2023-01-17 15:03:09 +0100

    1950-nél régebbi könyvek száma

--- src/main/java/com/example/konyvtarasztali/Statisztika.java
+++ src/main/java/com/example/konyvtarasztali/Statisztika.java
@@ -9,10 +9,15 @@ public class Statisztika {
     public static void main(String[] args) {
         try {
             beolvas();
-            System.out.printf("500 oldalnál hosszabb könyvek száma: %d\b", get500oldalnalHosszabbKonyvekSzama());
+            System.out.printf("500 oldalnál hosszabb könyvek száma: %d\n", get500oldalnalHosszabbKonyvekSzama());
+            System.out.printf("%s 1950-nél régebbi könyv\n", is1950nelRegebbi()? "Van" : "Nincs");
         } catch (SQLException e) {
             System.err.println("Hiba történt az adatbázis kapcsolat kialakításakor, a hiba: "+e.getMessage());
         }
+    }
+
+    private static boolean is1950nelRegebbi() {
+        return konyvek.stream().anyMatch(konyv -> konyv.getPublish_year() < 1950);
     }
 
     private static long get500oldalnalHosszabbKonyvekSzama() {
